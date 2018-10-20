b576bb5aae7f69d388f628beb7cd42ae315a57c7
Yujian Zhang <zyj200619477@gmail.com>
2018-10-20 18:43:36 -0500

    xml updated 3

--- app/src/main/java/huang/bling/smartbus/ui/Passenger/PassengerMainActivity.java
+++ app/src/main/java/huang/bling/smartbus/ui/Passenger/PassengerMainActivity.java
@@ -1,7 +1,17 @@
 package huang.bling.smartbus.ui.Passenger;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.CallSuper;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import huang.bling.smartbus.R;
 
@@ -10,9 +20,72 @@ public class PassengerMainActivity extends AppCompatActivity implements Passenge
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_passenger_main);
+        setContentView(R.layout.activity_main_passenger);
+        initView();
+    }
+    private void initView(){
+        Toolbar toolBar= (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(toolBar);
+        toolBar.setTitleTextColor(getResources().getColor(R.color.white));
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main_drawer,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.start:
+                break;
+            case R.id.end:
+                break;
+            case R.id.nav_about:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    /**
+     * 获取点击事件
+     */
+    @CallSuper
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_MOVE ) {
+            View view = getCurrentFocus();
+            if (isShouldHideKeyBord(view, ev)) {
+                hideSoftInput(view.getWindowToken());
+            }
+        }
+        return super.dispatchTouchEvent(ev);
     }
 
+    /**
+     * 判定当前是否需要隐藏
+     */
+    protected boolean isShouldHideKeyBord(View v, MotionEvent ev) {
+        if (v != null && (v instanceof EditText)) {
+            int[] l = {0, 0};
+            v.getLocationInWindow(l);
+            int left = l[0], top = l[1], bottom = top + v.getHeight(), right = left + v.getWidth();
+            return !(ev.getX() > left && ev.getX() < right && ev.getY() > top && ev.getY() < bottom);
+        }
+        return false;
+    }
+
+    /**
+     * 隐藏软键盘
+     */
+    private void hideSoftInput(IBinder token) {
+        if (token != null) {
+            InputMethodManager manager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            manager.hideSoftInputFromWindow(token, InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+    }
+
+    private void initListener(){
+
+    }
     @Override
     public void updateUi(PassengerData data) {
 
