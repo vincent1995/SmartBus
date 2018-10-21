d0eeb5ecd76e1670709b045c3ecd6690d642c2cb
Andrew Dai <madandrew666@gmail.com>
2018-10-20 20:23:22 -0500

    update NetInterface Andrew

--- app/src/androidTest/java/util/JSONHandler.java
+++ app/src/androidTest/java/util/JSONHandler.java
@@ -1,26 +1,29 @@
 package util;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import bean.Bean;
 import bean.DriverBean;
 import bean.PassagerBean;
 
 public class JSONHandler {
+    public static Gson myGson=new GsonBuilder().create();
     public static String DriverBeanToString(DriverBean bean){
 
-        return null;
+        return myGson.toJson(bean);
     }
 
     public static String PassagerBeanToString(PassagerBean bean){
 
-        return null;
+        return myGson.toJson(bean);
     }
     public static DriverBean StringToDriverBean(String str){
-        DriverBean result=new DriverBean();
-        result.setNBusStopLocation(5);
-        return result;
+
+        return myGson.fromJson(str,DriverBean.class);
     }
     public static PassagerBean StringToPassagerBean(String str){
 
-        return null;
+        return myGson.fromJson(str,PassagerBean.class);
     }
 }
